c6e010c2e76cc307cf2fca39b9025a4f6dd9f160
Boluwatife Victor <95125924+BirdboyBolu@users.noreply.github.com>
2022-04-14 08:10:32 +0100

    init

---
+++ src/Data.js
@@ -0,0 +1,27 @@
+const Data = {
+  products: [
+    {
+      id: '1',
+      name: 'Macbook',
+      price: 2599.99,
+      image:
+        'https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1hY2Jvb2slMjBwcm98ZW58MHx8MHx8&w=1000&q=80',
+    },
+    {
+      id: '2',
+      name: 'Ultra HD Television',
+      price: 3999.99,
+      image:
+        'https://images.unsplash.com/photo-1601944179066-29786cb9d32a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c21hcnQlMjB0dnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      id: '3',
+      name: 'Play Station 5',
+      price: 799.99,
+      image:
+        'https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGxheXN0YXRpb24lMjA1fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    },
+  ],
+};
+
+export default Data;
